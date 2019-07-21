413c7c6a85376602ee71e6d8043021d2aedd06a7
Kevin Arthur <kevin.arthur.roy@gmail.com>
2019-07-21 10:06:52 -0700

    add static files

--- api/index.js
+++ api/index.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = 3000;
 
 const { BASE_URI } = require("./CONSTANTS");
-
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 const { smartcarRouter, scheduleRouter } = require("./routes");
 
 app.use(bodyParser.json());
 app.use(`${BASE_URI}/smartcar`, smartcarRouter);
 app.use(`${BASE_URI}/schedule`, scheduleRouter);
-
-app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
